92b7f7a965b5efb1f4198f6c154dc63e068990d7
Timur <bolotovtmr@gmail.com>
2022-03-12 19:58:55 +0600

    fix: add base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import windi from 'vite-plugin-windicss'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), windi()],
-  base: '/'
+  base: '/dar-internship-bravo/'
 })
